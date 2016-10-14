c3b7d67ac14e4353cf5a2af4ee7283d32eef38e2
Albert Lo <ajlo@ucsd.edu>
2016-10-14 01:13:03 +0000

    finished bitcounts

--- bits.c
+++ bits.c
@@ -206,9 +206,17 @@ int logicalShift(int x, int n) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+/*Uses divide and conquer to count the number of bits.
+ *Adds the bits and places the sum in the
+ *Lower bits of the 2,4,8,16,32  positions.
+ */
+  x = (x & 0x55555555) + ((x>>1) & (0x55555555);
+  x = (x & 0x33333333) + ((x>>2) & (0x33333333);
+  x = (x & 0x0F0F0F0F) + ((x>>4) & (0x0F0F0F0F);
+  x = (x & 0x00FF00FF) + ((x>>8) & (0x00FF00FF);
+  x = (x & 0x0000FFFF) + ((x>>16) & (0x0000FFFF);
+  return x;
 }
-/* 
  * bang - Compute !x without using !
  *   Examples: bang(3) = 0, bang(0) = 1
  *   Legal ops: ~ & ^ | + << >>
